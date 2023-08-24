023b4c06c1f2df2739c9e140f267bc339c8c4069
vibhor <vibhor27061995@gmail.com>
2023-08-25 00:38:13 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['bayut-production.s3.eu-central-1.amazonaws.com']//act as an api
+  }
 }
 
 module.exports = nextConfig
