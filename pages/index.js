d834159321683762f8e30916adab8d0fbbcde1c1
vibhor <vibhor27061995@gmail.com>
2023-08-25 23:39:59 +0530

    Home page commit

--- pages/index.js
+++ pages/index.js
@@ -55,7 +55,9 @@ export default function Home({propertiesForSale, propertiesForRent}) {
         imageurl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
         
         />
+      <Flex flexWrap="wrap">
         {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
+      </Flex>
     </Box>
   )
 }
