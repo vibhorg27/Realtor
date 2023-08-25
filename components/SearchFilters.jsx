e58af06f1aea72324278e4f7fa9a5cb121e266c4
vibhor <vibhor27061995@gmail.com>
2023-08-26 01:03:29 +0530

    Search page commit

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import { Flex, Box, Text, Icon, Select, Input, Spinner, Button } from '@chakra-ui/react';
+import { useRouter } from "next/router";
+import { MdCancel } from 'react-icons/md';
+import Image from "next/image";
+
+const SearchFilters =() => {
+    return(
+        <div>
+            search Filters
+        </div>
+    )
+};
+
+export default SearchFilters;
