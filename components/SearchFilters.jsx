671d14718ec00b7a426334b228a50100ef0aa6a4
vibhor <vibhor27061995@gmail.com>
2023-08-26 14:48:41 +0530

    Search filter commit

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -4,11 +4,42 @@ import { useRouter } from "next/router";
 import { MdCancel } from 'react-icons/md';
 import Image from "next/image";
 
+import { filterData, getFilterValues } from "@/utils/filterData";
 const SearchFilters =() => {
+    const router = useRouter();
+    const [filters, setFilters] = useState(filterData);
+
+    const searchProperties = (filterValues) =>{
+        const path = router.pathname;
+        const { query } = router;
+
+        const values = getFilterValues(filterValues);
+
+        values.forEach((item) => {
+            query[item.name] = item.value
+        })
+
+        router.push({ pathname: path, query})
+    }
     return(
-        <div>
-            search Filters
-        </div>
+        <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
+            {filters.map((filter) => (
+                <Box key={filter.queyName}>
+                    <Select 
+                        placeholder={filter.placeholder}
+                        w="fit-content"
+                        p="2"
+                        onChange={(e) => searchProperties({[filter.queyName] : e.target.value})}>
+                        
+                        {filter?.items?.map((item) => (
+                            <option value={item.value} hey={item.value}>{item.name}
+                            </option>
+                        ))}
+                    </Select>
+
+                </Box>
+            ))}
+        </Flex>
     )
 };
 
